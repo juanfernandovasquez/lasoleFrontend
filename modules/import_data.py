ce4a44e4022ea59a1c10f23e6bc656a44e4852aa
Juan Vásquez <juanvasquezvergara@gmail.com>
2023-01-25 16:55:35 -0500

    agregué la carpeta data que finge ser mi base de datos

--- modules/import_data.py
+++ modules/import_data.py
@@ -3,7 +3,7 @@ import json
 
 def get_data():
 
-    ruta_data = "../data/productos.json"
+    ruta_data = "./data/productos.json"
     with open(ruta_data) as data:
         data = json.load(data)
 
